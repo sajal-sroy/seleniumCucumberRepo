521b2e4797f275042981f47aafbd7e6a4d209c39
Sinha Roy <sajal.sinha.roy@accenture.com>
2019-02-04 20:52:51 +0530

    changes in step file

--- CocumberGFTA/src/test/java/Step_Defs/Step_Defcodelogin.java
+++ CocumberGFTA/src/test/java/Step_Defs/Step_Defcodelogin.java
@@ -14,7 +14,7 @@ public class Step_Defcodelogin {
 public void open_Chrome_and_start_the_application() throws Throwable {
 	 System.setProperty("webdriver.chrome.driver","C:\\SajalData\\chromedriver.exe");
 	   driver=new ChromeDriver();
-	   driver.get("http://localhost:9002/mytestapp/login.htm");
+	   driver.get("http://localhost:9002/testmeapp/login.htm");
 	   driver.manage().window().maximize();
 	   
  
