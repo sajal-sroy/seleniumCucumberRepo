76941b280db2f920a59e320735f94916daf144f4
Sinha Roy <sajal.sinha.roy@accenture.com>
2019-02-04 22:44:43 +0530

    change in test runner

--- CocumberGFTA/src/test/java/Test_Runners/TestRunnerStep.java
+++ CocumberGFTA/src/test/java/Test_Runners/TestRunnerStep.java
@@ -6,7 +6,11 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="C:\\jenkins_workspace\\CocumberGFTA\\src\\test\\java",glue={"Step_Defs"})
+@CucumberOptions(features="C:\\jenkins_workspace\\CocumberGFTA\\src\\test\\java",
+                 glue={"Step_Defs"},
+                		 plugin = { "pretty", "json:target/cucumber-reports/Cucumber.json" },
+                		 monochrome = true)
 public class TestRunnerStep {
 
 }
+
